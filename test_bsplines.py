a4aafb78cf443dd13f1818e28a0cf5448f18e794
Paidi Creed <paidi.creed@benevolent.ai>
2018-11-07 10:19:04 +0000

    Add test for compute_quantile_knots

--- test_bsplines.py
+++ test_bsplines.py
@@ -1,8 +1,20 @@
 import numpy as np
 from bsplines import QuantileBSplineBasis
+from bsplines.bsplines import compute_quantile_knots
 
 
 def test_quartile_bspline():
     x = np.array(range(10))
     basis = QuantileBSplineBasis(0, 10, x, 3, 3)
     assert basis.design_matrix(x).shape == (10, 3)
+
+
+def test_compute_quantile_knots():
+    upper = 100
+    x1 = np.array(range(upper))
+    x2 = np.array(range(int(upper / 2)))
+    n_bases = 8
+    degree = 3
+    k1 = compute_quantile_knots(0, upper, x1, n_bases, degree)
+    k2 = compute_quantile_knots(0, upper, x2, n_bases, degree)
+    np.testing.assert_array_equal(k1, k2)
